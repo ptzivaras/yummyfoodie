63cda145b21aef6317165185c9f34b64455b1a71
ptzivaras <82588420+ptzivaras@users.noreply.github.com>
2025-04-13 23:50:07 +0300

    Header

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';  // Import ReactDOM
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
 
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);  // Now ReactDOM is defined
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 
 //powershell -ExecutionPolicy ByPass npm run dev
 //powershell -ExecutionPolicy ByPass
